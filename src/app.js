e2db2f8899d1d43c50dba3001ab00352da4456bc
quantum-35 <quantumcomputing35@icloud.com>
2018-10-19 13:24:19 +0300

    Add CSS loader to webpack to enable it run css and also update add script for starting server in the package.json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component} from "react";
+import "./App.css";
 
-template = <div>Welcome to Ride My way</div>
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Welcome to Ride My Way </h1>
+        <h2>Ride for fan, with friends</h2>
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(template, document.getElementById('app'));
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
+  entry: "./src/index.js",
+  mode: "development",
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
-  devtool: 'cheap-module-eval-source-map',
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
   devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
